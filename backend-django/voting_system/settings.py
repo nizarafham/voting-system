4de80d04d58f1f18370d6fae9c2e672e3d44fa8d
nizaru <117006245+nizarafham@users.noreply.github.com>
2025-02-06 09:09:23 +0700

    Update settings.py

--- backend-django/voting_system/settings.py
+++ backend-django/voting_system/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nizarafham17@gmail.com'
-EMAIL_HOST_PASSWORD = 'hxmz zobg rumm uovy'
+EMAIL_HOST_USER = 'email pengirim'
+EMAIL_HOST_PASSWORD = 'app password'
